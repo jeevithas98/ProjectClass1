c26961273178bff0f6aff516d21b9dd3d2592b74
jeevithas98 <jeevithast@gmail.com>
2023-04-30 06:33:38 +0530

    First time push

---
+++ src/test/java/org/tcs/ClassA.java
@@ -0,0 +1,17 @@
+package org.tcs;
+
+public class ClassA {
+	private void methodA() {
+		System.out.println("A");
+
+	}
+	private void methodB() {
+		System.out.println("B");
+
+	}
+	private void methodC() {
+		System.out.println("C");
+
+	}
+
+}
