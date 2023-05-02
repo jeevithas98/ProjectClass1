3a1732f95dc9740ad2eb6adf57b3968befb22fb0
jeevithas98 <jeevithast@gmail.com>
2023-05-02 08:11:06 +0530

    added 3 methods from red

--- src/test/java/org/tcs/ClassA.java
+++ src/test/java/org/tcs/ClassA.java
@@ -13,5 +13,17 @@ public class ClassA {
 		System.out.println("C");
 
 	}
+	private void tamilnadu() {
+		System.out.println("1");
+
+	}
+	private void kerala() {
+		System.out.println("1");
+
+	}
+	private void delhi() {
+		System.out.println("1");
+
+	}
 
 }
